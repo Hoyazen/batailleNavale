f164e7defd2d41d04c1bae454e88493460260548
Hoyazen <hoyazenn@gmail.com>
2026-02-02 11:17:06 +0100

    function displayGame - changing top and left index with the good ones

--- src/main/java/fr/adrien/Application.java
+++ src/main/java/fr/adrien/Application.java
@@ -14,16 +14,16 @@ public class Application {
 
     public static void displayGame() {
         char[][] display = new char[10][10];
-        int top = 0;
-        char left = 'A';
+        char top = 'A';
+        int left = 1;
         
         // aligne la ligne du haut avec le tableau (due au index de gauche (décalage de 1))
-        System.out.print("   ");
+        System.out.print("    ");
 
         // affichage ligne du haut à part du tableau
         for (int i = 0; i < display.length; i++) {
-            top = top + 1;
-            System.out.print(+ top + "  ");
+            System.out.print(top + "  ");
+            top = (char)(top + 1);
         }
 
         // saut le ligne pour séparer les index top du tableau
@@ -31,7 +31,8 @@ public class Application {
 
         // affichage intérieur du tableau
         for(int i = 0; i < display.length; i++) {
-            System.out.print((char)(left + i) + "  ");
+            System.out.printf("%2d  ", left); // le %2d me permet d'afficher un entier (d) sur une largeur de 2 caractères aligné à droite. pour éviter que le 10 ne décale tout le tableau
+            left = left + 1;
             for(int j = 0; j < display[i].length; j++) {
                 display[i][j] = '~';
                 System.out.print(display[i][j] + "  ");
