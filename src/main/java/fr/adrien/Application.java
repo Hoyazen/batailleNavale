8de99d234cf041491631e9d34e2f589c5b877c10
Hoyazen <hoyazenn@gmail.com>
2026-02-03 10:49:46 +0100

    reset projet ! - function initGrid and displayGrid to separate the logic; object added to get global variables and a better organization

--- src/main/java/fr/adrien/Application.java
+++ src/main/java/fr/adrien/Application.java
@@ -3,59 +3,34 @@ package fr.adrien;
 import java.util.Scanner;
 
 public class Application {
+    char[][] gridUser = new char[10][10];
+    char[][] gridAdmin = new char[10][10];
+    char gridWater = '~';
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println();
-        System.out.println("Voici mon jeu de bataille navale !");
-        System.out.println("Les navires ennemis sont générés aléatoirement");
-        System.out.println();
-        System.out.println("Pour commencer, voici un aperçu du plateau :");
-        System.out.println();
-
-        displayGame();
-        System.out.println();
-
-        gameLogic(scanner);
-
+        Application app = new Application();
+        app.initGrid();
+        app.displayGrid();
         scanner.close();
     }
 
-    public static void gameLogic(Scanner scanner) {
-        System.out.println("Sur quelles cases souhaitez-vous tirer ?");
-        System.out.println("Sélectionnez une lettre (POUR CHOISIR LA COLONNE (de A à J))");
-        System.out.println("Suivi d'un chiffre (POUR CHOISIR LA LIGNE (de 1 à 10)) :");
-        String columnUser = scanner.nextLine();
+    // seulement l'initialisation de l'eau dans la grille
+    public void initGrid() {
+        for(int i = 0; i < gridUser.length; i++) {
+            for(int j = 0; j < gridUser[i].length; j ++) {
+                gridUser[i][j] = gridWater;
+            }
+        }
     }
 
-    public static void displayGame() {
-        char[][] display = new char[10][10];
-        char top = 'A';
-        int left = 1;
-        
-        // aligne la ligne du haut avec le tableau (due au index de gauche (décalage de 1))
-        System.out.print("    ");
-
-        // affichage ligne du haut à part du tableau
-        for (int i = 0; i < display.length; i++) {
-            System.out.print(top + "  ");
-            top = (char)(top + 1);
-        }
-
-        // saut le ligne pour séparer les index top du tableau
-        System.out.println();
-
-        // affichage intérieur du tableau
-        for(int i = 0; i < display.length; i++) {
-            System.out.printf("%2d  ", left); // le %2d me permet d'afficher un entier (d) sur une largeur de 2 caractères aligné à droite. pour éviter que le 10 ne décale tout le tableau
-            left = left + 1;
-            for(int j = 0; j < display[i].length; j++) {
-                display[i][j] = '~';
-                System.out.print(display[i][j] + "  ");
+    // seulement l'affichage de la grille
+    public void displayGrid() {
+        for(int i = 0; i < gridUser.length; i++) {
+            for(int j = 0; j < gridUser[i].length; j ++) {
+                System.out.print(gridUser[i][j] + " ");
             }
             System.out.println();
         }
-        
     }
 }
